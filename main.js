f0dc444565d25c1ae6e3ec66fbc693f6b3cbe68a
Dhruv-Ayare <81720288+Dhruv-Ayare@users.noreply.github.com>
2022-08-28 12:51:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
 song2="";
+leftwristX=0;
+leftwristY=0;
+
+rightwrsitX=0;
+rightwrsitY=0;
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -10,8 +15,27 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        leftwristX=results[0].pose.leftWrist.x;
+        leftwristY=results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = "+leftwristX+", Left Wrist Y = "+leftwristY);
+
+        rightwristX=results[0].pose.rightWrist.x;
+        rightwristY=results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = "+rightwristX+", Right Wrist Y = "+rightwristY);
+    }
 }
